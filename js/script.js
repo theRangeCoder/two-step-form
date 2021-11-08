3a9e5d19fd6b15805d26114237101a725d66c3b8
Shuvadarshan Bhual <sbhual1998@gmail.com>
2021-11-08 07:20:07 +0100

    Experiment with client-side form validation

--- js/script.js
+++ js/script.js
@@ -40,47 +40,71 @@ previousBtn.addEventListener('click', () => {
     changeStep('previous');
 })
 
-// Client-side form input validation
-const first_name = document.getElementById('first-name');
-const last_name = document.getElementById('last-name');
-const email = document.getElementById('email');
-const number = document.getElementById('number');
-
-form.addEventListener('submit', (e) => {
-    // Prevent the form from submitting, so that the inputs are validated before it is submitted
-    e.preventDefault();
-    validateInputs();
-});
-
-// Create a function to display the message in case of an error
-const setError = (element, message) => {
-    // div with class input-control
-    const inputControl = element.parentElement;
-
-    // div, where the error message is displayed
-    const errorDisplay = inputControl.querySelector('.error');
-
-    // Set the error message and add the error class
-    errorDisplay.innerText = message;
-    inputControl.classList.add('error');
-    inputControl.classList.remove('success');
-}
-
-// Create a function to remove the input value if it is a valid input
-const setError = (element) => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
-    errorDisplay.innerText = '';
-    inputControl.classList.add('success');
-    inputControl.classList.remove('error');
-}
-
-const validateInputs = () => {
-    const first_name_value = first_name.value.trim();
-    const last_name_value = last_name.value.trim();
-    const email_value = email.value.trim();
-    const number_value = number.value.trim();
 
 
-}
+// // Client-side form input validation
+// const first_name = document.getElementById('first-name');
+// const last_name = document.getElementById('last-name');
+// const email = document.getElementById('email');
+// const number = document.getElementById('number');
+
+// form.addEventListener('submit', (e) => {
+//     // Prevent the form from submitting, so that the inputs are validated before it is submitted
+//     e.preventDefault();
+//     validateInputs();
+// });
+
+// // Create a function to display the message in case of an error
+// const setError = (element, message) => {
+//     // div with class input-control
+//     const inputControl = element.parentElement;
+
+//     // div, where the error message is displayed
+//     const errorDisplay = inputControl.querySelector('.error');
+
+//     // Set the error message and add the error class
+//     errorDisplay.innerText = message;
+//     inputControl.classList.add('error');
+//     inputControl.classList.remove('success');
+// }
+
+// // Create a function to remove the input value if it is a valid input
+// const setSuccess = (element) => {
+//     const inputControl = element.parentElement;
+//     const errorDisplay = inputControl.querySelector('.error');
+//     errorDisplay.innerText = '';
+//     inputControl.classList.add('success');
+//     inputControl.classList.remove('error');
+// }
+
+// const validateInputs = () => {
+//     const first_name_value = first_name.value.trim();
+//     const last_name_value = last_name.value.trim();
+//     const email_value = email.value.trim();
+//     const number_value = number.value.trim();
+
+//     if (first_name_value === '' || !first_name_value) {
+//         setError(first_name, 'First Name is required');
+//     } else {
+//         setSuccess(first_name);
+//     }
+
+//     if (last_name_value === '') {
+//         setError(last_name, 'Last Name is required');
+//     } else {
+//         setSuccess(last_name);
+//     }
+
+//     if (email_value === '') {
+//         setError(email, 'Email is required');
+//     } else {
+//         setSuccess(email);
+//     }
+
+//     if (number_value === '') {
+//         setError(number, 'Phone no. is required');
+//     } else {
+//         setSuccess(number);
+//     }
+// }
 
